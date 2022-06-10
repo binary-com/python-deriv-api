ad2811ae007820c389d842aa5fd7c0d1fbefe7ee
chylli-binary <52912308+chylli-binary@users.noreply.github.com>
2022-06-10 11:15:59 +0800

    Fixed a PyPI constraint where the package can only be installed on python ==3.9.6 (#7)

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## 0.1.1
+
+### Fixed:
+Fixed a PyPI constraint where the package can only be installed on python ==3.9.6
+
+
+## 0.1.0
+
+Initial version.
+
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='python_deriv_api',
     packages=find_packages(include=['deriv_api']),
-    version='0.1.0',
+    version='0.1.1',
     description='Python bindings for deriv.com websocket API',
     author='Deriv Group Services Ltd',
     author_email='learning+python@deriv.com',
@@ -14,10 +14,10 @@ setup(
     project_urls={
         'Bug Tracker': "https://github.com/binary-com/python-deriv-api/issues",
         'Documentation': "https://binary-com.github.io/python-deriv-api",
-        'Source Code': "https://github.com/binary-com/python-deriv-api"
-
+        'Source Code': "https://github.com/binary-com/python-deriv-api",
+        'Changelog': "https://github.com/binary-com/python-deriv-api/blob/master/CHANGELOG.md"
     },
-    python_requires="==3.9.6",
+    python_requires=">=3.9.6, !=3.9.7",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
